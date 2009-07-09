7d5c1622da758cecd6c79cb2c7bcb5bd673d74f0
Mikael Lind <elemel@elemel.se>
2009-07-09 21:38:13 +0200

    Configuration categories.

--- lib/nucleus/config.py
+++ lib/nucleus/config.py
@@ -1,40 +1,57 @@
 # encoding: UTF-8
 
+# Debug draw.
 debug_draw = False
 debug_color = 0, 255, 0
-rotate_letters = True
-scale_letters = True
+
+# Physics simulation.
+world_radius = 100.
+time_step = 1. / 60.
 friction = 1.
 restitution = 0.
 density = 1.
-circle_vertex_count = 64
-dictionary_file = '/usr/share/dict/swedish'
-dictionary_encoding = 'ISO-8859-1'
-alphabet = u'ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ'
-font_name = None
-font_bold = True
-font_scale = 1.
-time_step = 1. / 60.
-user_keys = {0x22: u'Å', 0x30: u'Ä', 0x2f: u'Ö'}
-fullscreen = True
-letter_count = 46
-view_height = 25.
 spring_constant = 10.
 damping = 5.
+
+# Presentation.
+rotate_letters = True
+scale_letters = True
 subpixel = True
+circle_vertex_count = 64
+fullscreen = True
+view_height = 25.
+
+# Gameplay.
+letter_count = 46
+hint = True
+time = 90.0
 creation_distance = 30.
 creation_interval = 0.1
 min_radius = 0.8
 max_radius = 1.2
+
+# Dictionary.
+dictionary_file = '/usr/share/dict/swedish'
+dictionary_encoding = 'ISO-8859-1'
+alphabet = u'ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ'
+pickle_file = 'nucleus.pickle'
+
+# Font.
+font_name = None
+font_bold = True
+font_scale = 1.
+
+# Input.
+user_keys = {0x22: u'Å', 0x30: u'Ä', 0x2f: u'Ö'}
+
+# Colors.
 color = 255, 255, 255
 background_color = 0, 0, 0
-hint = True
 hint_color = 0, 255, 255
-word_color = 0, 255, 0
 prefix_color = 255, 255, 0
+word_color = 0, 255, 0
 error_color = 255, 0, 0
-time = 90.0
-pickle_file = 'nucleus.pickle'
-world_radius = 100.
+
+# Labels.
 score_label = u'POÄNG'
 time_label = u'TID'
